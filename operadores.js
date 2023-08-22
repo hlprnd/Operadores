c7ae2c300d99a4756f2e81f65e7910e5e45e223f
Heloisa Prando <134503909+hlprnd@users.noreply.github.com>
2023-08-22 09:06:44 -0300

    Add files via upload

---
+++ operadores.js
@@ -0,0 +1,28 @@
+function somavalores(){
+    var val1 = document.getElementById("valor1").value;
+    var val2 = document.getElementById("valor2").value;
+    var val3 = parseInt(val1) + parseInt(val2);
+
+    alert(val3);
+}
+function subitraivalores(){
+    var val1 = document.getElementById("valor1").value;
+    var val2 = document.getElementById("valor2").value;
+    var val3 = parseInt(val1) - parseInt(val2);
+
+    alert(val3);
+}
+function multiplicavalores(){
+    var val1 = document.getElementById("valor1").value;
+    var val2 = document.getElementById("valor2").value;
+    var val3 = parseInt(val1) * parseInt(val2);
+
+    alert(val3);
+}
+function dividevalores(){
+    var val1 = document.getElementById("valor1").value;
+    var val2 = document.getElementById("valor2").value;
+    var val3 = parseInt(val1) / parseInt(val2);
+
+    alert(val3);
+}
